86ed05c352c4d32333cd7c704723dd4b4d9579a6
CaseyChien <caseychien89@gmail.com>
2025-05-16 10:55:13 +0800

    123

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,1 +1,17 @@
-print('hello1! flask!')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello, flask!</h1><p> firsit page</p>"
+
+
+@app.route("user")
+def user():
+    return "<h1>user!</h1><p> second page</p>"
+
+@app.route("product")
+def product():
+    return "<h1>product!</h1><p> third page</p>"
+
