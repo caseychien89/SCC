fc97e5da0f493aae2fbcccaf01cc32f3ec37b33b
CaseyChien <caseychien89@gmail.com>
2025-05-19 15:26:45 +0800

    123

--- lesson5/index.py
+++ lesson5/index.py
@@ -6,10 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html.jinja2")
 
-@app.route("/user")
-def user():
-    return "<h1>user!</h1><p>這是我的第2頁</p>"
 
-@app.route("/product")
-def product():
-    return "<h1>product!</h1><p>這是我的第3頁</p>"
+@app.route("/classes")
+def classes():
+    return render_template("classes.html.jinja2")
+
+
+@app.route("/new")
+def new():
+    return render_template("new.html.jinja2")
+
+@app.route("/traffic")
+def traffic():
+    return render_template("traffic.html.jinja2")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html.jinja2")
